aeea6d1a1b1d8958b4458e82228a573460812801
areedy <a.reedy@gmail.com>
2022-11-03 19:44:17 -0400

    adding js code to get zip code on button click, needs work still

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+//get zip code from button
+var zipInput = $("input");
+
+var zipSearch = function (event) {
+    event.preventDefault();
+    var zipResult = zipInput.val();
+    if (zipResult) {
+        getBreweries()
+    }
+}
+
+//get breweries function
+var getBreweries = function (zipcode){
+    var zipBrewerySearchApiUrl = 'https://api.openbrewerydb.org/breweries?by_postal=' + zipcode + '&per_page=3';
+    fetch(zipBrewerySearchApiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+            }
+        })
+}
+
+$("button").on(('click', getBreweries));
